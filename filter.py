4399c78710e44a9e56a5708841f6c5fd4215d299
LukMarks <lucas.mmoreno@hotmail.com>
2022-08-27 10:14:55 -0300

    Added object dependencies

--- filter.py
+++ filter.py
@@ -1,9 +1,14 @@
 # Auhor: Lucas Marques Moreno
 # Desriptiton: Kalman Filter
 
+from flight_simulator_interface import FlightSimulator
+from model import FlightModel
+
 class KalmanFilter:
     
-    def __init__(self) -> None:
+    def __init__(self, flight_model: FlightModel, simalution: FlightModel) -> None:
+        self.model = flight_model
+        self.simulation = simalution
         self.altitude = 0
     
     def altitude_estimation(self) -> None:
