7959664aedc7738278711c6ef415d7b5139558ad
LukMarks <lucas.mmoreno@hotmail.com>
2022-09-01 19:12:28 -0300

    Added property from flight data

--- flight_simulator_interface.py
+++ flight_simulator_interface.py
@@ -1,10 +1,37 @@
 # Auhor: Lucas Marques Moreno
 # Desriptiton: Flight Gear interface
 
+import pandas as pd
+import numpy as np
+
+
 class FlightSimulator:
+    def __init__(self, flight_data: str) -> None:
+        self.data = pd.read_csv(flight_data)
 
-    def __init__(self) -> None:
-        pass
+    @property
+    def simulation_time(self):
+        return self.data["_totl_time"]
+    
+    @property
+    def abs_speed(self):
+        return self.data["_Vind__mph"] * 1.6/3.6
 
-    def start_simulation(self) -> None:
-        pass
+    @property
+    def x_speed(self):
+        return self.data["___vX__m/s"]
+    
+    @property
+    def z_speed(self):
+        return self.data["___vZ__m/s"]
+
+    @property
+    def pitch_angle(self):
+        return self.data["pitch__deg"]
+    
+    @property
+    def altitude(self):
+        return self.data["__alt__ind"]
+    
+    def _mph_to_kmh(self, mph_speed):
+        return mph_speed*1.6
