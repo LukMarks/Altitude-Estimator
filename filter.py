Fixed filter covariance matrixes and gain

filter.py
@@ -3,45 +3,135 @@
 
 from flight_simulator_interface import FlightSimulator
 from model import FlightModel
-from scipy.integrate import odeint
+from scipy.integrate import odeint, solve_ivp
 import numpy as np
 
 
 class KalmanFilter:
     
-    def __init__(self, flight_model: FlightModel, simalution: FlightModel) -> None:
+    def __init__(self, flight_model: FlightModel, simalution: FlightSimulator) -> None:
 
         self.model = flight_model
         self.simulation = simalution
-    
+
+        self.altitude = []
+
+        self.u_calc = []
+        self.w_calc = []
+        self.q_calc = []
+        self.h_calc = []
+
+        self.state = []
+
+        self.A = np.identity(4)
+
+        sensor = {"u": np.sqrt(0.08*self.model.g),
+                  "w": np.sqrt(0.08*self.model.g),
+                  "q": np.sqrt(0.02),
+                  "h": np.sqrt(1)} # altimetro BMP180
+        
+        self.H = np.array([[0, 1, 0, 0],
+                           [0, 1, 0, 0],
+                           [0, 0, 1, 0],
+                           [0, 0, 0, 1]])
+
+        self.R = np.array([[np.random.normal(0,sensor["u"]), np.random.normal(0,sensor["w"]), np.random.normal(0,sensor["q"]), 0],
+                           [0, 0, 0, np.random.normal(0,sensor["h"])]])
+
+        self.Q =np.identity(4)*0 #np.array([[1, 1, 1, 0],
+                           #[0, 1, 0, 1]])
+
+        self.P =np.identity(4)# np.array([[1, 1, 1, 0],
+                           #[0, 0, 0, 1]])
+
+        self.zk = self.simulation.altitude    
+
     def set_filter_gain(self, gain) -> None:
         self.K = gain
 
-    def set_initial_state(self, state):
-        self.initial_state = state
+    def set_initial_state(self):
+        self.initial_state = [self.simulation.x_speed[0], self.simulation.z_speed[0], 
+                              self.simulation.pitch_angle[0], self.simulation.altitude[0]]
 
-    def altitude_a_priore(self) -> np.array:
-        solved = odeint(self.model._differential_model, self.initial_state, self.ode_time)
-        u = solved[:, 0]
-        w = solved[:, 1]
-        q = solved[:, 2]
-        h = solved[:, 3]
-        
-        return np.array([u, w, q, h])
+    def state_a_priore(self) -> None:
+
+        current_state = self.initial_state[0:3]
     
+        self.u_calc.append(self.initial_state[0])
+        self.w_calc.append(self.initial_state[1])
+        self.q_calc.append(self.initial_state[2])
+        self.h_calc.append(self.initial_state[3]) 
+        self.state.append(np.array([self.initial_state[0], self.initial_state[1], self.initial_state[2], self.initial_state[3]]))
+
+        for i in range(1,len(self.simulation.simulation_time)):
+            time_span = [self.simulation.simulation_time[i-1],self.simulation.simulation_time[i]]
+            solved = odeint(self.model._differential_model, current_state, time_span)
+            #solved = solve_ivp(self.model._differential_model, state, time_span)
+            self.u_calc.append(solved[1][0])
+            self.w_calc.append(solved[1][1])
+            self.q_calc.append(solved[1][2])
+
+            current_altitude = self.model.altitude_model(solved[1], self.h_calc[-1], time_span[0] - time_span[1])
+            self.h_calc.append(current_altitude)
+            
+
+            self.state.append( np.append(solved[1], current_altitude )   )
+            current_state = solved[1]
+
+    def state_a_priore_linearized(self) -> None:
+        
+        state = self.initial_state
+        self.u_calc.append(self.initial_state[0])
+        self.w_calc.append(self.initial_state[1])
+        self.q_calc.append(self.initial_state[2])
+        self.h_calc.append(self.initial_state[3]) 
+        self.state.append(np.array([self.initial_state[0], self.initial_state[1], self.initial_state[2], self.initial_state[3]]))
+
+        for i in range(1,len(self.simulation.simulation_time)):
+            time_span = [self.simulation.simulation_time[i-1],self.simulation.simulation_time[i]]
+            solved = odeint(self.model._linearized_model, state, time_span)
+            self.u_calc.append(solved[1][0])
+            self.w_calc.append(solved[1][1])
+            self.q_calc.append(solved[1][2])
+            self.h_calc.append(solved[1][3]) 
+
+            self.state.append(np.array(solved[1]))
+            state = solved[1]
+
     def set_ode_config(self, iterations: float, time_step: float) -> None:
         self.ode_time = np.linspace(0,time_step,iterations)
 
-    def altitude_a_posteriore(self) -> None:
-        state = self.altitude_a_priore() 
-        x_hat = state + self._calculate_filter_gain()*(zk - state * state)
+    def covariance_a_priore(self, state, P_old):
+        # print(np.matmul(np.matmul(self.A,P_old), self.A.transpose()))
+        # print(self.Q)
+        P = np.matmul(np.matmul(self.A,P_old), self.A.transpose()) + self.Q
+        return P
     
-    def _calculate_filter_gain(self) -> float:
-        K = P_prediction*state_transpose/(state*P_prediction*state_transpose + R)
+    def _calculate_filter_gain(self, P_minus) -> None:
+        aux = np.matmul(np.matmul(self.H, P_minus), self.H.transpose())# + self.R
+        #2x4 * 4x2 = 2x2 *  2x4 + 2x4 =2x4 
+        K = np.matmul(np.matmul(P_minus, self.H.transpose()), aux)
+        #2x4 . 4x2 = 2x2 * 2x4 = 2x4
         return K
     
-        
+    def generate_sensor_noise(self) -> None:
+        pass
 
-    # @property
-    # def altitude(self) -> float:
-    #     return self.altitude
+    def calc_altitude(self):
+
+        self.state_a_priore()#_linearized()
+        self.simulation.format_state()
+        P = self.P.transpose()
+        for iteration in range(0, len(self.simulation.simulation_time)):
+            state = [self.u_calc[iteration], self.w_calc[iteration], self.q_calc[iteration], self.h_calc[iteration]]
+            P = self.covariance_a_priore(state, P)
+            k = self._calculate_filter_gain(P)
+            
+            tmp =  self.simulation.state[iteration] - self.state[iteration] #np.matmul(self.H.transpose(),self.state[iteration].transpose())
+            temp2 = np.matmul(k.transpose(), tmp)
+            state_tmp = self.state[iteration] + temp2
+            self.altitude.append(-1*state_tmp[3])
+
+    @property
+    def get_altitude(self) -> float:
+        return self.altitude

flight_simulator_interface.py
@@ -1,5 +1,5 @@
 # Auhor: Lucas Marques Moreno
-# Desriptiton: Flight Gear interface
+# Desriptiton: Xplane 11 Data Handler
 
 import pandas as pd
 import numpy as np
@@ -8,30 +8,39 @@ import numpy as np
 class FlightSimulator:
     def __init__(self, flight_data: str) -> None:
         self.data = pd.read_csv(flight_data)
+        self.state = []
 
     @property
     def simulation_time(self):
-        return self.data["_totl_time"]
-    
+        return self.data["_totl_time"] 
+   
     @property
     def abs_speed(self):
         return self.data["_Vind__mph"] * 1.6/3.6
 
     @property
     def x_speed(self):
-        return self.data["___vX__m/s"]
+        return self.data["___vY__m/s"] + np.random.normal(0, (0.08*9.81)**2,len(self.data["_totl_time"]))
     
     @property
     def z_speed(self):
-        return self.data["___vZ__m/s"]
+        return -1*self.data["___vZ__m/s"] + np.random.normal(0,(0.08*9.81)**2,len(self.data["_totl_time"]))
 
     @property
     def pitch_angle(self):
-        return self.data["pitch__deg"]
+        return (self.data["pitch__deg"] + np.random.normal(0,np.sqrt(0.02),len(self.data["_totl_time"])))*np.pi/180
     
     @property
     def altitude(self):
-        return self.data["__alt__ind"]
-    
+        
+        #return self.data["____Z____m"] + np.random.normal(0,10,len(self.data["_totl_time"]))
+        return -1*self.data["__alt__ind"]*0.3048 + np.random.normal(0,np.sqrt(1),len(self.data["_totl_time"]))
+       
+
+    def format_state(self):
+        for row in range(0, len(self.data["_totl_time"])):
+            current_state = np.array([self.x_speed[row], self.z_speed[row], self.pitch_angle[row], self.altitude[row]])
+            self.state.append(current_state)
+ 
     def _mph_to_kmh(self, mph_speed):
         return mph_speed*1.6

model.py
@@ -11,8 +11,11 @@ class FlightModel:
         self.aircraft = aircraft
         self.environment = environment
 
-    def altitude_model(self) -> float:
-        pass
+    def altitude_model(self, state, altitude_ref, time_step) -> float:
+        h_dot = state[0]*np.sin(state[2]) - state[1]*np.cos(state[2])
+        altitude = altitude_ref + h_dot * time_step
+        return altitude
+        
 
     def _differential_model(self, state, t) -> np.array(float):
         """
@@ -21,22 +24,15 @@ class FlightModel:
             [u, w, q, h]
         """
 
-        V = np.sqrt(state[1]**2 + state[0]**2)
+        u_dot = state[2]*state[1] - self.g*np.sin(state[2]) + self.S*self.cx*0.5 * (state[1]**2 + state[0]**2) * self.p/self.m
 
-        pd = 0.5 * V**2 * self.p
+        w_dot = state[2]*state[0] + self.g*np.cos(state[2]) + self.S*self.cz*0.5 * (state[1]**2 + state[0]**2) * self.p/self.m
 
-        M = 0.5 * self.p * self.S * self.c * self.Cm * V**2
+        q_dot = (0.5 * self.p * self.S * self.c * self.Cm * (state[1]**2 + state[0]**2)) /self.Iy
 
-        u_dot = state[2]*state[1] - self.g*np.sin(state[2]) + pd*self.S*self.cx/self.m
+        #h_dot = state[0]*np.sin(state[2]) - state[1]*np.cos(state[2])
 
-        w_dot = state[2]*state[0] + pd*self.S*self.cz/self.m
-
-        q_dot = M /self.Iy
-
-        h_dot = state[0]*np.sin(state[2]) - state[1]*np.cos(state[2])
-
-        return u_dot, w_dot, q_dot, h_dot
-
+        return u_dot, w_dot, q_dot#, h_dot
     
     def _pd(self, u, w):
         """
@@ -54,16 +50,23 @@ class FlightModel:
             with the following state vector
             [u, w, q, h]
         """
-        self.A = np.array([
-                          [state[0]*self.p*self.cx/self.m, state[2] + state[1]*self.p*self.cx/self.m, -self.g*np.cos(state[2])+state[1], 0],
-                          [state[2] + state[0]*self.p*self.cz/self.m, state[1]*self.p*self.cz/self.m, state[0], 0],
-                          [state[0]*self.p*self.S*self.Cm*self.c/self.Iy, state[1]*self.p*self.S*self.Cm*self.c/self.Iy, 0, 0],
-                          [np.sin(state[2]), -np.cos(state[2]), state[0]*np.cos(state[2]) + state[1]*np.sin(state[2]), 0]
+        A = np.array([
+                          [state[0]*self.p*self.cx/self.m, state[2] + state[1]*self.p*self.cz/self.m, -self.g*np.cos(state[2])+state[1]],
+                          [state[2] + state[0]*self.p*self.cx/self.m, state[1]*self.p*self.cz/self.m,  -self.g*np.sin(state[2])+state[0]],
+                          [state[0]*self.p*self.S*self.Cm*self.c/self.Iy, state[1]*self.p*self.S*self.Cm*self.c/self.Iy, 0],
                           ])
 
-        x = np.matmul(self.A, state)
+        x = np.matmul(A, state)
         return x
 
+    def A_matrix(self, state):
+        A = np.array([
+                          [state[0]*self.p*self.cx/self.m, state[2] + state[1]*self.p*self.cz/self.m, -self.g*np.cos(state[2])+state[1]],
+                          [state[2] + state[0]*self.p*self.cx/self.m, state[1]*self.p*self.cz/self.m,  -self.g*np.sin(state[2])+state[0]],
+                          [state[0]*self.p*self.S*self.Cm*self.c/self.Iy, state[1]*self.p*self.S*self.Cm*self.c/self.Iy, 0],
+                          ])
+        return A
+        
     def set_aerodynamics_coefficient(self):
         
         self.Ct = 0
@@ -75,7 +78,7 @@ class FlightModel:
         self.c = self.aircraft.get("chord", 0)
         self.m = self.aircraft.get("mass", 0)
         self.Cl = self.aircraft.get("CL", 0)
-        self.Cd = 0.001625*self.Cl**3 + 0.30061*self.Cl**2 + 0.007446*self.Cl #self.aircraft.get("CD", 0)
+        self.Cd = 0.001625*self.Cl**3 + 0.30061*self.Cl**2 + 0.007446*self.Cl
         self.Cm = self.aircraft.get("CM", 0)
         self.alpha = self.aircraft.get("AoA", 0)
         self.Ix = self.aircraft.get("Inertia", [[0]])[0][0]